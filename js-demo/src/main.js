aa8253d65db52632fbfb9a3bcd825b8825d602e9
Taras Leskiv <leskiv.taras@gmail.com>
2025-04-13 13:14:15 +0300

    js

--- js-demo/src/main.js
+++ js-demo/src/main.js
@@ -1,24 +1,42 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+function setupCounter(element) {
+  let counter = 0
+  const setCounter = (count) => {
+    counter = count
+    element.innerHTML = `count is ${counter}`
+  }
+  element.addEventListener('click', () => setCounter(counter + 1))
+  setCounter(0)
+}
 
 setupCounter(document.querySelector('#counter'))
+
+const button = document.querySelector('#hello')
+button.addEventListener('click', () => {
+  button.innerHTML = 'Hello World! ' + Math.random()
+  button.classList.add('hello')
+})
+
+let myVariable = 4;
+
+const x = 2 + 2;
+
+if (x === 2) {
+  //some behavior;
+}
+
+const weaklyTypedVariable = 42 + "42";
+console.log('JavaScript is a weakly typed language,', weaklyTypedVariable);
+
+// Each variable has a type
+let myString = "Hello, world!";
+let myNumber = 42;
+console.log(typeof myString); // "string"
+console.log(typeof myNumber); // "number"
+console.log(typeof setupCounter) // "function"
+console.log(typeof button) // "object"
+
+// strict comparison and weak comparison
+console.log('2 == "2"', 2 == "2")
+console.log('2 === "2"', 2 === "2")
